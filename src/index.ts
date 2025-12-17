37ad05e200f472352bcd5a7a45d7642c7980d093
ryan-bennett <ryan.bennett.au@gmail.com>
2025-12-18 09:20:50 +1000

    fix: export schema store interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export { DraftId, DraftSchemas } from "./Drafts";
-export { SchemaRepository } from "./SchemaRepository";
-export { SerializableSchemaStore } from "./SerializableSchemaStore";
-export { validateSchema, validate } from "./SchemaUtils"
-export { JsonSchema, JsonError }  from "json-schema-library";
+export {DraftId, DraftSchemas} from "./Drafts";
+export {SchemaRepository} from "./SchemaRepository";
+export {SchemaStore} from "./SchemaStore";
+export {SerializableSchemaStore} from "./SerializableSchemaStore";
+export {validateSchema, validate} from "./SchemaUtils"
+export {JsonSchema, JsonError} from "json-schema-library";
