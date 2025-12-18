16d25a8cc177affd48bcb0b0724e86a2dec51256
ryan-bennett <ryan.bennett.au@gmail.com>
2025-12-18 15:35:16 +1000

    fix: trying to sort exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-export * from "./Drafts";
-export * from "./SchemaRepository";
-export * from "./SchemaStore";
-export * from "./SchemaVersion";
-export * from "./SerializableSchemaStore";
-export * from "./SchemaUtils"
+export {type DraftId, DraftSchemas} from "./Drafts";
+export {
+    SchemaRepository,
+    type SchemaRepositoryConfig,
+    SchemaCreateError,
+    SchemaCreateRequest,
+    type SchemaGetRequest,
+    type SchemaUpdateRequest,
+    type SchemaUpdateType,
+    SchemaRepositoryError,
+    SchemaUpdateError,
+    SchemaValidationError,
+    SchemaNotFoundError
+} from "./SchemaRepository";
+export {
+    SchemaStore,
+    type SchemaStoreGetRequest,
+    type SchemaStoreGetVersionsRequest,
+    type SchemaStorePutRequest,
+    type SchemaStoreGetLatestVersionsRequest
+} from "./SchemaStore";
+export {SchemaVersion} from "./SchemaVersion";
+export {SerializableSchemaStore} from "./SerializableSchemaStore";
+export {validateSchema, validate} from "./SchemaUtils"
