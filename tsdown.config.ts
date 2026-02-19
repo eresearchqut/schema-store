038b44a3a043c307b1ef60f3d2c0c1a5ecd5531b
ryan-bennett <ryan.bennett.au@gmail.com>
2026-02-19 16:21:24 +1000

    feat: tsup -> tsdown for packaging

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
     entry: ['src/index.ts'],
@@ -7,5 +7,6 @@ export default defineConfig({
     dts: true,
     clean: true,
     minify: false,
-    format: ['cjs', 'esm']
+    format: ['cjs', 'esm'],
+  target: false,
 })
