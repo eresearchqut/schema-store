0831af82310960c11d4eb61e026929d4ab8e4b8a
ryan-bennett <ryan.bennett.au@gmail.com>
2026-02-19 16:39:00 +1000

    fix: tsup -> tsdown for packaging

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'tsdown'
+import {defineConfig} from 'tsdown'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    splitting: true,
     sourcemap: true,
+
     dts: true,
     clean: true,
     minify: false,
     format: ['cjs', 'esm'],
-  target: false,
+    target: false,
+    exports: true
 })
